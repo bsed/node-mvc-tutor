feefc281a11f9b8da75dfed554f6b0348367dc7e
webfd <284178359@qq.com>
2015-09-08 11:46:57 +0800

    node demo for upload mvc

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var url = require("url");
+
+function start(route,handle) {
+	function onRequest(request,response){
+		var postData="";
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for "+pathname+" received.");
+		console.log("Request received.");
+		request.setEncoding("utf8");
+		
+		request.addListener("data",function(postDataChunk){
+			postData +=postDataChunk;
+			console.log("Received POST data chunk '"+postDataChunk+"'.");				
+		});
+		
+		request.addListener("end",function(){
+			route(handle,pathname,response,postData);
+		});
+		
+		//route(pathname,handle);
+		/*response.writeHead(200,{"Content-Type": "text/plain"});
+		var content =route(handle,pathname);
+		response.write(content);
+		response.end();*/
+	}
+	
+	http.createServer(onRequest).listen(8888);
+	console.log("Server hah started.");
+
+}
+exports.start = start;
